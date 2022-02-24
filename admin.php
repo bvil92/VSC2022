527bdab498e1b2c24e575210de9492b555dd0b7d
Beatrice Vilkaityte <beatrice.vilkaityte@gmail.com>
2022-02-24 23:02:23 +0200

    Bea Codes, she does

--- admin.php
+++ admin.php
@@ -7,6 +7,19 @@ if (array_key_exists('username', $_SESSION')) {
     echo    '<br><a href=logout.php>Atsijungti</a>;
 }
 
+include '..src/Entities/Car.php';
+
+$Car = new Car('green', '0 kmph', 10);
+
+echo 'We are driving' . $car->getColour() . ' car<br>';
+echo 'Current speed is: ' . $car->getCurrentSpeed() . '<br>';
+echo 'Current mileage is: ' . $car->getMileage() . '<br>';
+$amount = 10;
+echo  "Let's fill " . $amount . "litres of petrol<br>";
+$car->fillGasoline($amount);
+$newSpeed = "50 kmph";
+$car->changeColour('red')
+
 else {
     header('location: login.html');
     }
